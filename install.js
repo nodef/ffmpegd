3ef6fdac9c01b7d45a09303ee44bc4e493179c42
Subhajit Sahu <wolfram77@gmail.com>
2017-11-17 11:24:31 +0530

    :monkey_face: add urls

--- install.js
+++ install.js
@@ -1,4 +1,25 @@
+const download = require('download-progress/lib/download-progress');
+const unzip = require('unzip');
 const cp = require('child_process');
+const fs = require('fs');
+
+
+// 1. settings
+const url = {
+  'linux': {
+    'arm': 'https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-armel-32bit-static.tar.xz',
+    'arm64': 'https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-arm64-64bit-static.tar.xz',
+    'x86': 'https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-32bit-static.tar.xz',
+    'x64': 'https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-64bit-static.tar.xz'
+  },
+  'win32': {
+    'x86': 'https://ffmpeg.zeranoe.com/builds/win32/shared/ffmpeg-3.2-win32-shared.zip',
+    'x64': 'https://ffmpeg.zeranoe.com/builds/win64/shared/ffmpeg-3.2-win64-shared.zip'
+  },
+  'darwin': {
+    'x64': 'https://ffmpeg.zeranoe.com/builds/macos64/shared/ffmpeg-3.2-macos64-shared.zip'
+  }
+};
 
 
 try { cp.execSync('ffmpeg --help'); }
